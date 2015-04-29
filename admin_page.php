be28f3facb900c44b27f216d0b3698a334df2735
jalisap <jalisaperkins10@gmail.com>
2015-04-28 23:15:38 -0500

    Revert "updates to administration"
    This reverts commit f50395e0b2d52ee3eb61b549013b7d91d6cf9557.

--- admin_page.php
+++ admin_page.php
@@ -70,173 +70,18 @@ if(!empty($_POST)){
 $pagePermissions = fetchPagePermissions($pageId);
 $permissionData = fetchAllPermissions();
 
-
+require_once("models/header.php");
 
 echo "
-
-<!DOCTYPE html>
-<html lang='en-US'>
-<head>
-
-<title>PV Student Community</title>
-<style type='text/css'>
-body{
-
-background-image:url('resources/images/bg4.jpg') ;
-background-repeat:repeat;
-background-color:#40113f;
-font-family:'lucida sans unicode';
-font-size:11px;
-line-height:16px;
-text-align:justify;
-}
-
-#container{
-position:absolute;
-left:50%;
-margin-left:-392px;
-top:0px;
-width:775px;
-height:625px;
-background-image:url('resources/images/white.jpg');
-background-repeat:repeat;
-
-}
-
-.header{
-margin-top:40px;
-text-align:center;
-}
-
-.navigation{
-width:690px;
-padding:5px;
-margin-left:40px;
-margin-top:40px;
-margin-bottom:20px;
-text-align:center;
-
-}
-
-.title{
-font-family:'century gothic';
-font-size:23px;
-font-weight:bold;
-color:#40113f;
-display:block;
-text-align:left;
-margin-left:140px;
-margin-top:5px;}
-
-.sidebar{
-float:left;
-margin-left:40px;
-width:200px;
-}
-
-.main{
-float:right;
-margin-right:50px;
-_margin-right:30px;
-width:460px;
-}
-
-a.nav, a.nav:active, a.nav:visited, a.nav:link{
-color:#40113f;
-text-decoration:none;
-font-family:'century gothic';
-font-weight:bold;
-font-size:12px;
-padding-right:15px;
-}
-
-a.nav:hover{
-color:#785343;
-text-decoration:none;
-font-family:'century gothic';
-font-weight:bold;
-font-size:12px;
-padding-right:15px;
-}
-
-a.nav2, a.nav2:active, a.nav2:visited, a.nav2:link{
-color:#40113f;
-text-decoration:none;
-font-family:'century gothic';
-font-weight:bold;
-font-size:12px;
-}
-
-a.nav2:hover{
-color:#785343;
-text-decoration:none;
-font-family:'century gothic';
-font-weight:bold;
-font-size:12px;
-}
-
-a, a:active, a:visited, a:link{
-color:#dfc326;
-text-decoration:none;
-}
-
-a:hover{
-color:#dfc326;
-text-decoration:none;
-}
-
-b, strong{
-color:#dfc326;
-text-decoration:none;
-font-family:'century gothic';
-font-size:25px;
-}
-
-i, em{
-color:#dfc326;
-}
-
-u{
-border-bottom:1px dotted #dfc326;
-text-decoration:none;
-}
-
-
-h1{
-font-family:'century gothic';
-font-size:22px;
-text-align:left;
-letter-spacing:0px;
-color:#40113f;
-border-bottom:2px dotted #dfc326;
-padding-top:10px;
-margin-bottom:5px;
-padding-bottom:4px;
-}
-
-</style>
-</head>
-
 <body>
+<div id='wrapper'>
+<div id='top'><div id='logo'></div></div>
+<div id='content'>
+<h1>UserCake</h1>
+<h2>Admin Page</h2>
+<div id='left-nav'>";
 
-<div id='container'>
-
-<div class='header'>
-<img src='resources/images/profile.jpg'> 
-</div>
-
-<div class='navigation'>
-<a class='nav' href='account.php'>Account Home</a>
-<a class='nav' href='user_settings.php'>User Settings</a>
-<a class='nav' href='admin_users.php'>Admin Users</a>
-<a class='nav' href='admin_permissions.php'>Admin Permissions</a>
-<a class='nav' href='admin_configuration.php'>Admin Config</a>
-<a class='nav' href='admin_pages.php'>Admin Pages</a>
-<a class='nav' href='logout.php'>Logout</a>
-<br>
-<br>
-
-";
+include("left-nav.php");
 
 echo "
 </div>
@@ -245,7 +90,6 @@ echo "
 echo resultBlock($errors,$successes);
 
 echo "
-<center>
 <form name='adminPage' action='".$_SERVER['PHP_SELF']."?id=".$pageId."' method='post'>
 <input type='hidden' name='process' value='1'>
 <table class='admin'>
@@ -298,7 +142,6 @@ foreach ($permissionData as $v1) {
 
 echo"
 </p>
-</center>
 </div>
 </td>
 </tr>
